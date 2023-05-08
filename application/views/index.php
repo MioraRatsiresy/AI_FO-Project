9d8bc6dc010eb3f12e6dc463b161d82846ea252f
MioraRatsiresy <99732281+MioraRatsiresy@users.noreply.github.com>
2023-05-09 00:30:39 +0300

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -133,7 +133,7 @@
 
                     <div class="col-lg-6 col-md-12 wow zoomIn" data-wow-delay="0.3s">
                         <a class="position-relative d-block overflow-hidden" href="">
-                            <img class="img-fluid" src="img/cat-2.jpg" alt="">
+                            <img class="img-fluid" src="<?php echo $categorie['imagecategorie'];?>" alt="">
                             <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3"
                                 style="margin: 1px;">
                                 <h5 class="m-0">
